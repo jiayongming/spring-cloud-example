7de95230f0286f9b992b951b3ec868fc478efacb
ymjia <ymjia@rxhui.com>
2019-02-25 15:33:42 +0800

    add lombok

--- eureka-server/src/main/java/com/example/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eureka/server/EurekaServerApplication.java
@@ -1,14 +1,17 @@
 package com.example.eureka.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication //等同于@Configuration　+　@EnableAutoConfiguration和 　+　@ComponentScan
+@Slf4j
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		log.info("「「「「「Eureka服务注册中心启动完成」」」」」");
 	}
 }
